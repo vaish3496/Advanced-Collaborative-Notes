8765a1367e1bbb5d2206255bea2b1c8110e7b2c5
vaish3496 <vaishnavdamani3496@gmail.com>
2020-11-13 21:37:42 +0530

    Minor changes

--- views/notes.js
+++ views/notes.js
@@ -6,15 +6,9 @@ socket.on('update_notes_for_new_users', (notes_text) =>{
     textarea_notes.value = notes_text;
 })
 
-var timeoutRef;
 
 textarea_notes.addEventListener('keyup' , () => {
-    if(timeoutRef){
-        clearTimeout(timeoutRef)
-    }    
-    timeoutRef =  document.setTimeout(() => {
-        socket.emit('update_notes_for_current_users',textarea_notes.value)
-    }, 2000);
+    socket.emit('update_notes_for_current_users',textarea_notes.value)
 })
 
 socket.on('update_notes_for_current_users',(notes_text) =>{
