27fe7dcfb3013594593a0ddf186a7f63f24aa414
vaish3496 <vaishnavdamani3496@gmail.com>
2020-11-13 22:21:01 +0530

    Minor changes

--- views/notes.js
+++ views/notes.js
@@ -10,9 +10,10 @@ socket.on('update_notes_for_new_users', (notes_text) =>{
 
 
 textarea_notes.addEventListener('keyup' , () => {
-    setTimeout(() => {
-        socket.emit('update_notes_for_current_users',textarea_notes.value)
-    }, 1500);
+    // setTimeout(() => {
+    //     socket.emit('update_notes_for_current_users',textarea_notes.value)
+    // }, 1500);
+    socket.emit('update_notes_for_current_users',textarea_notes.value)
 })
 
 socket.on('update_notes_for_current_users',(notes_text) =>{
