d04856501620423dd05faadbba52e955154482c3
vaish3496 <vaishnavdamani3496@gmail.com>
2020-11-17 19:38:05 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -3,30 +3,60 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 const io = require('socket.io')(server)
-
+const {v4:uuid4} = require('uuid')
 
 const port = process.env.PORT || 8800
 
 
-
-
 app.use(express.static(__dirname+'/views'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
 
 app.get('/', (req,res) =>{
     res.render('index')
 })
 
-var notes_text
+app.get('/home', (req,res) =>{
+    res.render('home')
+})
+
+app.get('/:id', (req,res) => {
+    res.render('index')
+})
+
+
+
 
 
 
 io.sockets.on('connection', socket => {
-    io.sockets.emit('update_notes_for_new_users',notes_text)
+    // io.sockets.emit('update_notes_for_new_users',notes_text)
 
-    socket.on('update_notes_for_current_users',(data) =>{
-        notes_text = data
-        socket.broadcast.emit('update_notes_for_current_users',notes_text)
+    socket.on('update_notes_for_current_users',(data,roomId) =>{
+        // console.log(roomId)
+        // socket.join(roomId)
+        console.log(data)
+        io.to(roomId).emit('update_notes_for_current_users',data)
+        // console.log(data,roomId,socket.id)
+        var clients = io.sockets.adapter.rooms;
+        console.log(clients)
+        console.log(socket.id)
     })
+
+    socket.on('create_new_room', (roomId) =>{
+        // const roomId = uuid4()
+        socket.join(roomId)
+        // io.to(roomId).emit('send-roomId',roomId)
+        console.log("user created room",socket.id)
+    })
+
+    socket.on('join_room', (roomId) =>{
+        socket.join(roomId)
+        // io.to(roomId).emit('send-roomId',roomId)
+        console.log("user joined room",socket.id)
+    })
+
 })
 
 
--- views/notes.js
+++ views/notes.js
@@ -1,22 +1,57 @@
-const socket = io('https://collaborative-notes.herokuapp.com/')
-// const socket = io('http://localhost:8800')
+// const socket = io('https://collaborative-notes.herokuapp.com/')
+const socket = io('http://localhost:8800')
+const getLastItem = thePath => thePath.substring(thePath.lastIndexOf('/') + 1)
 
-var textarea_notes = document.getElementById('textarea_notes')
+
+window.onload = ()=>{
+    var create_room = document.getElementById('create_room')
+    var join_room = document.getElementById('join_room')
+    var textarea_notes = document.getElementById('textarea_notes')
+    // if(create_room){
+    //     create_room.addEventListener('click', () =>{
+    //         console.log('creating new room')
+    //         socket.emit('create_new_room')
+    //     });
+    // }
+
+    if(join_room){
+        join_room.addEventListener('click', () =>{
+            const join_roomId = prompt('Enter Room ID')
+            // socket.emit('join_room',join_roomId)
+            window.location.href = `http://localhost:8800/${join_roomId}`
+        })
+        
+    }
+
+    if(textarea_notes){
+        socket.emit('create_new_room',getLastItem(window.location.href))
+        socket.emit('join_room',getLastItem(window.location.href))
+        console.log('new room created after redirect')
+        textarea_notes.addEventListener('keyup' , () => {
+            // setTimeout(() => {
+            //     socket.emit('update_notes_for_current_users',textarea_notes.value)
+            // }, 1500);
+            socket.emit('update_notes_for_current_users',textarea_notes.value,getLastItem(window.location.href))
+        })
+    }
+
+}
 
 
-socket.on('update_notes_for_new_users', (notes_text) =>{
+
+socket.on('send-roomId', roomId =>{
+    console.log('send-roomId')
+    window.location.href = `http://localhost:8800/${roomId}`
+})
+
+
+
+socket.on('update_notes_for_new_users', notes_text =>{
     textarea_notes.value = notes_text;
 })
 
 
-textarea_notes.addEventListener('keyup' , () => {
-    // setTimeout(() => {
-    //     socket.emit('update_notes_for_current_users',textarea_notes.value)
-    // }, 1500);
-    socket.emit('update_notes_for_current_users',textarea_notes.value)
-})
-
-socket.on('update_notes_for_current_users',(notes_text) =>{
+socket.on('update_notes_for_current_users',notes_text =>{
     textarea_notes.value = notes_text;
-    console.log(notes_text)
+    console.log(notes_text,'are we here?',textarea_notes.value)
 })
